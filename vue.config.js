80dd889cead15e7cc8938a1b147b7695a91f17ce
Your Name <you@example.com>
2019-09-06 08:56:56 +0800

    再来

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//   // 测试, 写了一个注释, 测试 git push
+//   devServer: {
+//     port: 3000,
+//     open: true
+//   }
+// }
+module.exports = {
+  devServer: {
+    port: 3000,
+    open: true
+  }
+}
